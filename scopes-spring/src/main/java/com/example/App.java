45ebbd691a73264866dd2aae505ac3bbc81fb1d6
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2022-11-09 16:10:21 +0100

    first

---
+++ scopes-spring/src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.setMessage("I'm object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+//        objB.setMessage("I am Abderrahim");
+        objB.getMessage();
+    }
+}
---
+++ scopes-spring/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+}
