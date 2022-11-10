de7bb74944d34799b36cc57adc6f4661827d3f1d
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2022-11-10 10:22:09 +0100

    adding bean definition with inner bean

--- scopes-spring/src/main/java/com/example/App.java
+++ scopes-spring/src/main/java/com/example/App.java
@@ -10,13 +10,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld per = (HelloWorld) context.getBean("helloWorld");
 
-        objA.setMessage("I'm object A");
-        objA.getMessage();
+        per.getMessage();
 
-        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-//        objB.setMessage("I am Abderrahim");
-        objB.getMessage();
-    }
+        }
 }
---
+++ scopes-spring/src/main/java/com/example/Person.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Person {
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
